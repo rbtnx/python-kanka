d7063e2c64058be8498d49f0cd23401623eaf9d0
rbtnx <kathrin.weihe@posteo.de>
2020-05-07 14:19:13 +0200

    added Family, Organisation and Item entities

--- kanka/objects/entities.py
+++ kanka/objects/entities.py
@@ -22,6 +22,9 @@ class Character(Entity):
         self._traits = [Trait(t) for t in traits]
 
         super(Character, self).__init__(data)
+    
+    def __repr__(self):
+        return "Character: {} (id: {})".format(self._name, self._id)
 
     @property
     def location_id(self):
@@ -106,6 +109,9 @@ class Location(Entity):
 
         super(Location, self).__init__(data)
 
+    def __repr__(self):
+        return "Location: {} (id: {})".format(self._name, self._id)
+
     @property
     def parent_location_id(self):
         """
@@ -129,3 +135,130 @@ class Location(Entity):
         :rtype: string
         """
         return self._map
+    
+class Family(Entity): 
+    """
+    Family entity.
+    """
+    def __init__(self, data):
+        self._location_id = data["location_id"]
+        self._family_id = data["family_id"]
+        
+        members = data["members"]["data"]
+        self._members = [Character(c) for c in members]
+
+        super(Family, self).__init__(data)
+    
+    def __repr__(self):
+        return "Family: {} (id: {})".format(self._name, self._id)
+
+    @property
+    def location_id(self):
+        """
+        :return: Location ID
+        :rtype: integer
+        """
+        return self._location_id
+
+    @property
+    def family_id(self):
+        """
+        :return: Family ID
+        :rtype: integer
+        """
+        return self._family_id
+
+    @property
+    def members(self):
+        """
+        :return: Family members
+        :rtype: list of Entity::Character
+        """
+        return self._members
+
+class Organisation(Entity): 
+    """
+    Organisation entity.
+    """
+    def __init__(self, data):
+        self._location_id = data["location_id"]
+        self._organisation_id = data["family_id"]
+        self._type = data["type"]
+        
+        members = data["members"]["data"]
+        self._members = [Character(c) for c in members]
+
+        super(Organisation, self).__init__(data)
+    
+    def __repr__(self):
+        return "Organisation: {} (id: {})".format(self._name, self._id)
+
+    @property
+    def location_id(self):
+        """
+        :return: Location ID
+        :rtype: integer
+        """
+        return self._location_id
+
+    @property
+    def organisation_id(self):
+        """
+        :return: Organisation ID
+        :rtype: integer
+        """
+        return self._organisation_id
+
+    @property
+    def type(self):
+        """
+        :return: Organisation type
+        :rtype: string
+        """
+        return self._type
+
+    @property
+    def members(self):
+        """
+        :return: Family members
+        :rtype: list of Entity::Character
+        """
+        return self._members
+    
+class Item(Entity):
+    """
+    Item entity.
+    """
+    def __init__(self, data):
+        self._location_id = data["location_id"]
+        self._character_id = data["character_id"]
+        self._type = data["type"]
+        
+        super(Item, self).__init__(data)
+    
+    def __repr__(self):
+        return "Item: {} (id: {})".format(self._name, self._id)
+    
+    @property
+    def location_id(self):
+        """
+        :return: Location ID
+        :rtype: integer
+        """
+        return self._location_id
+    
+    @property
+    def character_id(self):
+        """
+        :return: Character ID
+        :rtype: integer
+        """
+        return self._character_id
+    
+    @property
+    def type(self):
+        """
+        :return: Item type
+        :rtype: string
+        """
+        return self._type
